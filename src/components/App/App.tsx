2ad03fb3b0773e6f964f5b6a874cc01fa827fe8f
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-27 13:51:00 +0100

    Prvi koraci

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Container } from 'react-bootstrap'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function App() {
+  return (
+    <Container>
+      <FontAwesomeIcon icon={faHome}/> Home
+    </Container>
+  );
+}
+
+export default App;

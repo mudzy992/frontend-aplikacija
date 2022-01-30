ed9b71f4b466c75eb4a7413e8216214b3ad05a86
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-01-30 14:53:40 +0100

    Proces se nastavlja u narednom branchu - Login

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,46 +1,165 @@
-import axios, { AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { ApiConfig } from '../config/api.config'
 
 export default function api(
     path: string, 
     method: 'get' | 'post' | 'patch' | 'delete',
     body: string | undefined,
+    role: 'user' | 'administrator' = 'user',
     ){
-        return new Promise((resolve, reject) =>{
-         axios({
+        return new Promise<ApiResponse>((resolve) =>{
+         const requestData = {
             method: method,
             url: path,
             baseURL: ApiConfig.API_URL,
             data: JSON.stringify(body),
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': getToken()
+                'Authorization': getToken(role)
             }
-        }).then(res => responseHandler(res, resolve, reject)) 
-        .catch(err => reject(err)) 
+        }
+        axios(requestData)
+        .then(res => responseHandler(res, resolve, requestData)) 
+        .catch(err => {
+            const response: ApiResponse = {
+                status: 'error',
+                data: err
+            }
+            resolve(response)
+        })
     })    
 }
 
-function getToken(): string {
-    const token = localStorage.getItem('api_token');
-
-    return 'Berer ' + token
+interface ApiResponse{
+    status: 'ok' | 'error' | 'login';
+    data: any;
 }
 
-function saveToken(token: string){
-    localStorage.setItem('api_token', token)
-}
-
-function responseHandler(
+async function responseHandler(
     res: AxiosResponse<any>,
-    resolve:(value?:unknown) => void, 
-    reject: (reason?:any)=>void
+    resolve:(value?: ApiResponse) => void,
+    requestData: { /* AxiosRequestConfig: any, */
+        
+        method: "get" | "post" | "patch" | "delete";
+        url: string;
+        baseUrl: string;
+        data: string;
+        headers: {
+            'Content-Type': string;
+            Authorization: string;
+        }
+    }
     ){
         if(res.status < 200 || res.status >= 300){
-            return reject(res.data)
+
+            if(res.status ===  401){
+                const newToken = await refreshToken(requestData)
+
+                if(!newToken){
+                    const response: ApiResponse = {
+                        status: 'login',
+                        data: null,
+                    }
+                    return resolve(response)
+                }
+
+                saveToken(newToken)
+
+                requestData.headers['Authorization'] = getToken();
+
+                return await repeatRequest(requestData, resolve);
+            }
+
+            const response: ApiResponse ={
+                status: 'error',
+                data: res.data,
+            }
+            return resolve(response)
         }
+
+        let response: ApiResponse;
         if(res.data.statusCode < 0){
-            return reject(res.data)
+            response = {
+                status: 'login',
+                data: null,
+            }
+            } else {
+                response = {
+                    status: 'ok',
+                    data: res.data,
+                }
+            }
+
+            resolve(response)
         }
-        resolve(res.data)
+
+
+    function getToken(role: 'user' | 'administrator'): string {
+        const token = localStorage.getItem('api_token' + role);
+        return 'Berer ' + token;
+    }
+    
+    
+    function saveToken(token: string){
+        localStorage.setItem('api_token', token)
+    }
+
+    function getRefreshToken(role: 'user' | 'administrator'): string {
+        const token = localStorage.getItem('api_refresh_token' + role);
+        return token + '';
+    }
+
+    function saveRefreshToken(token: string){
+        localStorage.setItem('api_refresh_token', token)
+    }
+
+    async function refreshToken(role: 'user' | 'administrator'): Promise<string | null> {
+        const path = 'auth/' + role + '/refresh';
+        const data = {
+            token: getRefreshToken(role),
+        }
+    
+        const refreshTokenRequestData: AxiosRequestConfig = {
+            method: 'post',
+            url: path,
+            baseURL: ApiConfig.API_URL,
+            data: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    
+        const rtr: { data: { token: string | undefined } } = await axios(refreshTokenRequestData);
+    
+        if (!rtr.data.token) {
+            return null;
+        }
+    
+        return rtr.data.token;
+    }
+    async function repeatRequest(requestData: AxiosRequestConfig, resolve: (value?: ApiResponse)=> void){
+        axios(requestData)
+        .then(res => {
+            let response: ApiResponse;
+            if(res.status === 401){
+                response = {
+                    status: 'login',
+                    data: null,
+                }
+            } else {
+                response = {
+                    status: 'ok',
+                    data: res,
+                }
+            }
+
+            return resolve(response)
+        })
+        .catch(err => {
+            const response: ApiResponse = {
+                status: 'error',
+                data: err
+            }
+            return resolve(response)
+        })
     }
